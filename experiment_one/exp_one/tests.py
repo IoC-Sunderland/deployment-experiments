5943a1e9d6b242fac8ff0b3698e638b540a0cac9
IoC-Sunderland <gavin.mcclary@sunderland.ac.uk>
2020-12-12 16:46:15 +0000

    Installed Selenium and added to requirements.txt, also added two tests to tests.py

--- experiment_one/exp_one/tests.py
+++ experiment_one/exp_one/tests.py
@@ -1,3 +1,11 @@
+from selenium import webdriver
 from django.test import TestCase
 
-# Create your tests here.
+class AppInitialisationTests(TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_home_in_title_of_default_route(self):
+        self.driver.get('http://127.0.0.1:8000')
+        self.assertIn("Home", self.driver.title)
