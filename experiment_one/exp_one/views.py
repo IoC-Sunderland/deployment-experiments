3cd875139b417b19488fcea0d9e120b7d5bb7aa0
IoC-Sunderland <gavin.mcclary@sunderland.ac.uk>
2020-12-12 16:36:25 +0000

    Rendering index.html template at root path

--- experiment_one/exp_one/views.py
+++ experiment_one/exp_one/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    
+    return render(request, 'exp_one/index.html', {
+        'Project Name': 'experiment_one',
+    })
+
