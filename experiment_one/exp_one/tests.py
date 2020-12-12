82bf2168a51ad32a0f347675fe45fdbfe55f106d
IoC-Sunderland <gavin.mcclary@sunderland.ac.uk>
2020-12-12 17:11:08 +0000

    Added table to index.html template

--- experiment_one/exp_one/tests.py
+++ experiment_one/exp_one/tests.py
@@ -1,11 +1,21 @@
 from selenium import webdriver
 from django.test import TestCase
 
-class AppInitialisationTests(TestCase):
+class HomePageTests(TestCase):
 
+    # User goes to www.gavmac.com
     def setUp(self):
         self.driver = webdriver.Chrome()
-
-    def test_home_in_title_of_default_route(self):
         self.driver.get('http://127.0.0.1:8000')
+
+    def tearDown(self):
+        self.driver.close()
+
+    # User expects to see Home in title
+    def test_home_in_title_of_default_route(self):
         self.assertIn("Home", self.driver.title)
+
+    # User expects to see a username and password box with Login button
+    def test_login_form_exists_on_page(self):
+        pass
+
