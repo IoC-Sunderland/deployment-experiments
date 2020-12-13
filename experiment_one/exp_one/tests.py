1dfee9703a7c078535327de522f0aadee7c8541f
IoC-Sunderland <gavin.mcclary@sunderland.ac.uk>
2020-12-13 10:53:27 +0000

    Tests for empty username and password fields - small amends to index.html

--- experiment_one/exp_one/tests.py
+++ experiment_one/exp_one/tests.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from django.test import TestCase
+import time
+
+DELAY = 2
 
 class HomePageTests(TestCase):
 
-    # User goes to www.gavmac.com
+    # Gavin goes to www.gavmac.com
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.get('http://127.0.0.1:8000')
@@ -11,11 +14,50 @@ class HomePageTests(TestCase):
     def tearDown(self):
         self.driver.close()
 
-    # User expects to see Home in title
+    # Gavin expects to see Home in title
     def test_home_in_title_of_default_route(self):
         self.assertIn("Home", self.driver.title)
 
-    # User expects to see a username and password box with Login button
+    # Gavin expects to see a username and password box with Login button
     def test_login_form_exists_on_page(self):
-        self.driver.find_element_by_class_name('container')
+        self.driver.find_element_by_id('username')
+        self.driver.find_element_by_id('password')
+        self.driver.find_element_by_id('submit')
+    
+    # Gavin expects to be able to enter username and password
+    def test_able_to_enter_username_and_password(self):
+        username = self.driver.find_element_by_id('username')
+        username.send_keys('Gavin')
+        password = self.driver.find_element_by_id('password')
+        password.send_keys('TestPass')
+        time.sleep(DELAY)
 
+    def test_message_shown_if_username_AND_password_empty_when_login_button_clicked(self):
+        username = self.driver.find_element_by_id('username')
+        password = self.driver.find_element_by_id('password')
+        username.clear()
+        password.clear()
+        login_button = self.driver.find_element_by_id('submit')
+        login_button.click()
+        message = self.driver.find_element_by_class_name('warning')
+        self.assertEqual(message.text, 'Missing fields for username, password')
+
+    def test_message_shown_if_username_field_is_empty_when_login_button_clicked(self):
+        username = self.driver.find_element_by_id('username')
+        username.clear()
+        password = self.driver.find_element_by_id('password')
+        password.send_keys('TestPass')
+        login_button = self.driver.find_element_by_id('submit')
+        login_button.click()
+        message = self.driver.find_element_by_class_name('warning')
+        self.assertEqual(message.text, 'Missing fields for username')
+    
+    def test_message_shown_if_password_field_is_empty_when_login_button_clicked(self):
+        username = self.driver.find_element_by_id('username')
+        username.send_keys('Gavin')
+        password = self.driver.find_element_by_id('password')
+        password.clear()
+        login_button = self.driver.find_element_by_id('submit')
+        login_button.click()
+        message = self.driver.find_element_by_class_name('warning')
+        self.assertEqual(message.text, 'Missing fields for password')
--- experiment_one/exp_one/views.py
+++ experiment_one/exp_one/views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages 
 
 def index(request):
     
-    return render(request, 'exp_one/index.html', {
+    if request.method == "POST":
+
+        details = {}
+
+        details['username'] = request.POST.get('username','')
+        details['password'] = request.POST.get('password','')
+
+        print(details)
+
+        missing = list()
+
+        for k, v in details.items():
+            if v == "":
+                missing.append(k)
+
+        if missing:
+            messages.add_message(
+                request, messages.WARNING, f"Missing fields for {', '.join(missing)}")
+            return render(request, "exp_one/index.html")
+
+        new_user = Users(username=details['username'],
+                         email=details['email'],
+                        password=details['password'])
+
+        print(new_user)
+
+        new_user.save()
+
+        return redirect('/')
+
+    return render(request, "exp_one/index.html", {
         'Project Name': 'experiment_one',
     })
-
