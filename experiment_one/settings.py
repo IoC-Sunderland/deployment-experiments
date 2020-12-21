b92035dcec1eb59ffe41af5c955ad7a48e774120
IoC-Sunderland <gavin.mcclary@sunderland.ac.uk>
2020-12-21 17:16:07 +0000

    Amended ALLOWED_HOSTS again...

--- experiment_one/settings.py
+++ experiment_one/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'https://ioc-experiment-one.herokuapp.com/']
+ALLOWED_HOSTS = ['0.0.0.0', 'ioc-experiment-one.herokuapp.com/']
 
 
 # Application definition
