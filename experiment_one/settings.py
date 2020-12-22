41793627e7d6d7e989445e131e2a8ce1816d5950
IoC-Sunderland <gavin.mcclary@sunderland.ac.uk>
2020-12-22 11:11:51 +0000

    Created superuser

--- experiment_one/settings.py
+++ experiment_one/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'ioc-experiment-one.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'ioc-experiment-one.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
